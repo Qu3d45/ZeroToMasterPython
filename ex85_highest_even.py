b9b323b8b5746f45949307cacd71692e2acc1087
Lameira <lameira.manuel@gmail.com>
2019-11-14 20:47:43 +0000

    ex85

--- ex85_highest_even.py
+++ ex85_highest_even.py
@@ -1,4 +1,9 @@
 def highest_even(li):
-    pass
+    evens = []
+    for item in li:
+        if item % 2 == 0:
+            evens.append(item)
+    return max(evens)
 
-print(highest_even([10,2,3,4,8,11]))
+
+print(highest_even([10, 2, 3, 4, 8, 11]))
