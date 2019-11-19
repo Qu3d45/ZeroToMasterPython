039d2fd6e6cb544f48c1a2e40ddf7a1a04f768c0
Lameira <lameira.manuel@gmail.com>
2019-11-19 22:36:28 +0000

    add121

---
+++ ex111_oop.py
@@ -0,0 +1,17 @@
+﻿class PlayerCharacter():
+    def __init__(self, name, age):
+        self.name = name  # attributes
+        self.age = age
+
+    def run(self):
+        print('run')
+        return 'done'
+
+
+player1 = PlayerCharacter('manuel', 37)
+player2 = PlayerCharacter('berto', 25)
+
+print(player1.name)
+print(player1.age)
+
+print(player1.run())
---
+++ ex121_inheritance.py
@@ -0,0 +1,29 @@
+﻿class User():
+    def sign_in(self):
+        print("Logged in")
+
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f"attacking with power of {self.power}")
+
+
+class Archer(User):
+    def __init__(self, name, number_arrows):
+        self.name = name
+        self.number_arrows = number_arrows
+
+    def attack(self):
+        print(f"attacking with arrows: arrows left {self.number_arrows}")
+
+
+wizard1 = Wizard("Merlin", 50)
+print(wizard1.sign_in())
+wizard1.attack()
+
+archer1 = Archer("Robin", 100)
+archer1.attack()
